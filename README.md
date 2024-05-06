f2f68b5e3eef24384252a3520da1158fd41e2b0f
yaoqijun <yaoqijunmail@gmail.com>
2024-05-06 22:43:13 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SourceFrontAnalyse
 前端分析工具
+
+1. template 基于vue 模版框架, 参考： https://github.com/LiveAlone/BasicFrontTemplate
+2. webpackage 文档内容梳理。 https://github.com/LiveAlone/WebpackStarter
+3. example 前端项目构建方式 集成配置方式
---
+++ webpack_first/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    devtool: 'inline-source-map',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '管理输出',
+        }),
+    ],
+    devServer: {
+        static: './dist'
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        }
+    },
+    module: {
+        rules: [
+            // {
+            //     test: /\.css$/i,
+            //     use: ['style-loader', 'css-loader'],
+            // },
+            // {
+            //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            //     type: 'asset/resource',
+            // },
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            //     type: 'asset/resource',
+            // },
+            // {
+            //     test: /\.(csv|tsv)$/i,
+            //     use: ['csv-loader'],
+            // },
+            // {
+            //     test: /\.xml$/i,
+            //     use: ['xml-loader'],
+            // },
+        ]
+    }
+};
